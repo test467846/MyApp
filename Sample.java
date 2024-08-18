b2b307d3c89c776da051413028c8eb6fd39cff7d
test467846 <145253220+test467846@users.noreply.github.com>
2024-08-18 14:01:49 +0530

    add one print statement

--- Sample.java
+++ Sample.java
@@ -4,6 +4,8 @@ public class Sample{
 	public static void main(String[] args){
 
 		System.out.println("This is my first program....");
+		System.out.println("This is my second program....");
+		
 
 	}
 
